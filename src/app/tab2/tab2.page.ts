998e2fb0157e9442201bb5ca1435ccdcfb72d28a
mathcorrea <matheus.correa58@etec.sp.gov.br>
2022-10-03 20:37:38 -0300

    botões

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  operacao = '';
+  resultado = '0';
+
   constructor() {}
+
+  ngOnInit() {}
+
+  adicionarValor(valor: any){
+    if(valor >= 0) // Não poder digitar dois símbolos
+    this.operacao += valor;
+  }
+
+  limparMemoria(){
+    this.operacao = '';
+    this.resultado = '';
+  }
+
+  limparOperacao(){
+    this.operacao = '';
+  }
+
+  inverterValor(){
+    // Alterna o número entre positivo e negativo
+  }
+
+  apagarCaracter(){
+    if(this.operacao.length > 0) {
+    this.operacao = this.operacao.substring(0, this.operacao.length - 1) }
+   }
+
 
 }
